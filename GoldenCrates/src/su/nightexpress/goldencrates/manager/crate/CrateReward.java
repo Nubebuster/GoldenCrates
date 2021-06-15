5eca8af83345c2ca2cabd0940cf83e4c2c46ffa7
Mark <kiwiszijncool@gmail.com>
2021-06-15 02:48:13 +0200

    Apply reward name to preview item

--- GoldenCrates/src/su/nightexpress/goldencrates/manager/crate/CrateReward.java
+++ GoldenCrates/src/su/nightexpress/goldencrates/manager/crate/CrateReward.java
@@ -7,6 +7,7 @@ import java.util.UUID;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -22,37 +23,37 @@ public class CrateReward implements Editable, Cleanable {
 
 	private final Crate crate;
 	private final String id;
-	
+
 	private double chance;
 	private String name;
-	
+
 	private ItemStack item;
 	private List<String> cmd;
 	private ItemStack preview;
-	
+
 	private CrateEditorReward editor;
-	
+
 	public CrateReward(@NotNull Crate crate) {
 		this(
-			crate,										// Holder
-			UUID.randomUUID().toString(),				// ID
-			
-			25D,										// Chance
-			"New Reward #" + crate.getRewards().size(), // Name
-			
-			new ItemStack(Material.APPLE)	,			// Preview
-			new ItemStack(Material.APPLE), 				// Item
-			new ArrayList<>()							// Commands
+				crate,                                        // Holder
+				UUID.randomUUID().toString(),                // ID
+
+				25D,                                        // Chance
+				"New Reward #" + crate.getRewards().size(), // Name
+
+				new ItemStack(Material.APPLE),            // Preview
+				new ItemStack(Material.APPLE),                // Item
+                new ArrayList<>()                            // Commands
 		);
 	}
 
 	public CrateReward(
 			@NotNull Crate crate,
 			@NotNull String id,
-			
+
 			double chance,
 			@NotNull String name,
-			
+
 			@Nullable ItemStack preview,
 			@Nullable ItemStack item,
 			@NotNull List<String> cmd
@@ -61,12 +62,12 @@ public class CrateReward implements Editable, Cleanable {
 		this.id = id.toLowerCase();
 		this.setChance(chance);
 		this.setName(name);
-		
+
 		this.setItem(item);
 		this.setCommands(cmd);
 		this.setPreview(preview);
 	}
-	
+
 	@Override
 	@NotNull
 	public CrateEditorReward getEditor() {
@@ -88,61 +89,66 @@ public class CrateReward implements Editable, Cleanable {
 	public Crate getCrate() {
 		return this.crate;
 	}
-	
+
 	@NotNull
 	public String getId() {
 		return this.id;
 	}
-	
+
 	@NotNull
 	public String getName() {
 		return this.name;
 	}
-	
+
 	public void setName(@NotNull String name) {
 		this.name = StringUT.color(name);
 	}
-	
+
 	public double getChance() {
 		return this.chance;
 	}
-	
+
 	public void setChance(double chance) {
 		this.chance = chance;
 	}
-	
+
 	@Nullable
 	public ItemStack getItem() {
 		return this.item == null ? null : new ItemStack(this.item);
 	}
-	
+
 	public void setItem(@Nullable ItemStack item) {
 		this.item = item == null ? null : new ItemStack(item);
 	}
-	
+
 	@NotNull
 	public List<String> getCommands() {
 		return this.cmd;
 	}
-	
+
 	public void setCommands(@NotNull List<String> cmd) {
 		this.cmd = new ArrayList<>(cmd);
 	}
-	
+
 	@NotNull
 	public ItemStack getPreview() {
 		return new ItemStack(this.preview);
 	}
-	
+
 	public void setPreview(@Nullable ItemStack item) {
 		if (item == null) {
 			this.preview = this.item != null ? this.getItem() : new ItemStack(Material.BARRIER);
-		}
-		else {
-			this.preview = new ItemStack(item);
+		} else {
+			if (!item.getItemMeta().hasDisplayName()) {
+				ItemMeta im = item.getItemMeta();
+				im.setDisplayName(getName());
+				this.preview = new ItemStack(item);
+				this.preview.setItemMeta(im);
+			} else
+				this.preview = new ItemStack(item);
 		}
 	}
-	
+
 	public void give(@NotNull Player player) {
 		ItemStack item = this.getItem();
 		if (item != null) {
